1d1ddf54ef4dafbe4a8d963ba9711e71ec8bef08
David Schleef <ds@entropywave.com>
2011-12-04 16:07:41 -0800

    Replace old ew-stream-server program with new

--- tools/ew-stream-server.c
+++ tools/ew-stream-server.c
@@ -1,146 +1,147 @@
+/* EwEncoder
+ * Copyright (C) 2011 David Schleef <ds@schleef.org>
+ * Copyright (C) 2010 Entropy Wave Inc
+ *
+ */
 
+#ifdef HAVE_CONFIG_H
 #include "config.h"
-
-#include <gst/gst.h>
-#include <libsoup/soup.h>
-#include <gst-stream-server/gss-server.h>
-
-#if 0
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
 #endif
 
-#define GETTEXT_PACKAGE NULL
+#include "ew-stream-server.h"
 
-gboolean verbose = FALSE;
-char *hostname = NULL;
+#include <gst/gst.h>
 
-void handle_pipeline_message (GstBus *bus, GstMessage *message,
-    gpointer user_data);
-GstElement * create_pipeline (int i);
-void init_server (void);
-gboolean timeout (gpointer data);
-void client_removed (GstElement *e, int arg0, int arg1,
-    gpointer user_data);
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-GMainLoop *main_loop;
 
-int listen_socket;
+#define GETTEXT_PACKAGE "ew-streaming-server"
 
-static GOptionEntry entries[] =
-{
-  { "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, "Be verbose", NULL },
+//#define CONFIG_FILENAME "/opt/entropywave/ew-oberon/config"
+#define CONFIG_FILENAME "config"
 
-  { "hostname", 'h', 0, G_OPTION_ARG_STRING, &hostname, "Hostname to use in URLs", NULL },
+extern EwConfigDefault config_defaults[];
 
-  { NULL }
+gboolean verbose;
+gboolean cl_verbose;
+
+void ew_stream_server_notify_url (const char *s, void *priv);
+
+static void signal_interrupt (int signum);
+
+#define N_PROGRAMS 10
+
+EwProgram *programs[N_PROGRAMS];
+
+static GOptionEntry entries[] = {
+  {"verbose", 'v', 0, G_OPTION_ARG_NONE, &cl_verbose, "Be verbose", NULL},
+
+  {NULL}
+
 };
+
+#define N_ENCODERS 1
+EwServer *server;
+GMainLoop *main_loop;
 
 int
 main (int argc, char *argv[])
 {
   GError *error = NULL;
   GOptionContext *context;
-  EwServer *server;
-  EwProgram *program;
   int i;
 
-  if (!g_thread_supported ()) g_thread_init(NULL);
+  if (!g_thread_supported ())
+    g_thread_init (NULL);
 
-  context = g_option_context_new ("[HOSTS] - stream server relay");
+  signal (SIGPIPE, SIG_IGN);
+  signal (SIGINT, signal_interrupt);
+
+  context = g_option_context_new ("- FIXME");
   g_option_context_add_main_entries (context, entries, GETTEXT_PACKAGE);
   g_option_context_add_group (context, gst_init_get_option_group ());
   if (!g_option_context_parse (context, &argc, &argv, &error)) {
-    g_print ("option parsing failed: %s\n", error->message);
+    g_print ("option parsing failed: %s", error->message);
     exit (1);
   }
   g_option_context_free (context);
 
-  if (argc < 2) {
-    g_print ("expected addresses of hosts to follow\n");
-    exit (1);
-  }
-
   server = ew_server_new ();
+  ew_server_read_config (server, CONFIG_FILENAME);
 
-  if (hostname) {
-    ew_server_set_hostname (server, hostname);
+  ew_config_set_config_filename (server->config, CONFIG_FILENAME);
+  ew_config_load_defaults (server->config, config_defaults);
+  ew_config_load_from_file (server->config);
+  ew_config_load_from_file_locked (server->config, CONFIG_FILENAME ".perm");
+  ew_config_load_from_file_locked (server->config, CONFIG_FILENAME ".package");
+
+  for(i=0;i<N_PROGRAMS;i++){
+    char *key;
+
+    key = g_strdup_printf("stream%d_url", i);
+
+    ew_config_set_notify (server->config, key,
+        ew_stream_server_notify_url, GINT_TO_POINTER (i));
+
+    ew_stream_server_notify_url (key, GINT_TO_POINTER (i));
+
+    g_free (key);
   }
 
-  for(i=1;i<argc;i++){
-    char *stream_name;
-    if (i == 1) {
-      stream_name = g_strdup ("stream");
-    } else {
-      stream_name = g_strdup_printf ("stream%d", i-1);
-    }
-    program = ew_server_add_program (server, stream_name);
-    ew_program_follow (program, argv[i], "stream");
-    g_free (stream_name);
-  }
+  //g_timeout_add (1000, periodic_timer, NULL);
 
-  main_loop = g_main_loop_new(NULL, TRUE);
+  main_loop = g_main_loop_new (NULL, TRUE);
 
-  g_timeout_add (1000, (GSourceFunc)timeout, server);
-
-  g_main_loop_run(main_loop);
+  g_main_loop_run (main_loop);
 
   ew_server_free (server);
   ew_server_deinit();
   gst_deinit();
-  
-  return 0;
+
+  exit (0);
 }
 
-
-
-gboolean
-event_probe_callback (GstPad *pad, GstMiniObject *mini_obj, gpointer user_data)
+static void
+signal_interrupt (int signum)
 {
-  GstEvent *event;
-
-  event = GST_EVENT (mini_obj);
-
-  g_print("event %s\n", gst_event_type_get_name (event->type));
-
-  return TRUE;
+  if (main_loop) {
+    g_main_loop_quit (main_loop);
+  }
 }
 
-
-gboolean
-timeout (gpointer data)
-{
-  EwServer *server = (EwServer *) data;
-
-#if 0
-  {
-    static int countdown = 90;
-    if (countdown-- == 0) {
-      g_main_loop_quit (main_loop);
-    }
-  }
-#endif
-
-  if (0 && verbose) {
-    int n_clients = 0;
-    int i;
-    EwProgram *program = server->programs[0];
-
-    for(i=0;i<program->n_streams;i++){
-      n_clients += program->streams[i]->n_clients;
-    }
-    g_print ("clients: %d\n", n_clients);
-  }
-
-  return TRUE;
-}
 
 void
-ew_server_add_admin_callbacks (EwServer *server, SoupServer *soupserver)
+ew_stream_server_notify_url (const char *s, void *priv)
 {
+  int i = GPOINTER_TO_INT (priv);
+  const char *url;
+
+  url = ew_config_get (server->config, s);
+
+  if (url[0] == 0 && programs[i] == NULL) return;
+  if (programs[i] && strcmp(url, programs[i]->follow_host) == 0) return;
+
+  if (programs[i]) {
+    ew_server_remove_program (server, programs[i]);
+    programs[i] = NULL;
+  }
+
+  if (url[0]) {
+    char *stream_name;
+    if (i == 0) {
+      stream_name = g_strdup ("stream");
+    } else {
+      stream_name = g_strdup_printf ("stream%d", i);
+    }
+    programs[i] = ew_server_add_program (server, stream_name);
+    ew_program_follow (programs[i], url, "stream");
+    g_free (stream_name);
+  }
+
 }
+
 
