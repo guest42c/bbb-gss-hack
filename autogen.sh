ccc666b00d7bdeeeb62243bb8e22c60ef82b3818
David Schleef <ds@schleef.org>
2012-05-12 16:54:57 -0700

    Enable gtk-doc build and commit hook

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+if [ `whoami` = ds ] ; then
+  confargs="$confargs --enable-gtk-doc"
+fi
+
+# install pre-commit hook for doing clean commits
+if test ! \( -x .git/hooks/pre-commit -a -L .git/hooks/pre-commit \);
+then
+    rm -f .git/hooks/pre-commit
+    ln -s ../../misc/pre-commit.hook .git/hooks/pre-commit
+fi
+
 autoreconf -i -f
 #patch -p0 <patch-configure
-./configure $confflags $@
+./configure $confargs $@
 
