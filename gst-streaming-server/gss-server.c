abd00a874c6eab1cc87b2688ac60922bed60a728
David Schleef <ds@schleef.org>
2012-10-18 22:28:02 -0700

    enable flowplayer

--- gst-streaming-server/gss-server.c
+++ gst-streaming-server/gss-server.c
@@ -232,6 +232,7 @@ gss_server_init (GssServer * server)
   server->enable_rtsp = DEFAULT_ENABLE_RTSP;
   server->enable_rtmp = DEFAULT_ENABLE_RTMP;
 
+  server->enable_flowplayer = TRUE;
   server->enable_programs = TRUE;
   server->programs = NULL;
   server->archive_dir = g_strdup (DEFAULT_ARCHIVE_DIR);
