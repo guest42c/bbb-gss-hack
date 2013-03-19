4309f4a1294bdd0687b1d32f69bfb02277d7f781
David Schleef <ds@schleef.org>
2013-03-18 23:33:43 -0700

    config: Fix previous commit

--- gst-streaming-server/gss-config.c
+++ gst-streaming-server/gss-config.c
@@ -304,14 +304,14 @@ gss_config_get_post_hash (GssTransaction * t)
 
   hash = NULL;
   if (g_ascii_strncasecmp (content_type, "application/x-www-form-urlencoded",
-          33)) {
+          33) == 0) {
     hash = soup_form_decode (t->msg->request_body->data);
-  } else if (g_ascii_strncasecmp (content_type, "multipart/form-data", 19)) {
-    SoupBuffer *buffer;
+  } else if (g_ascii_strncasecmp (content_type, "multipart/form-data", 19) == 0) {
+    SoupBuffer *buffer = NULL;
 
     hash = soup_form_decode_multipart (t->msg, "none", NULL, NULL, &buffer);
 
-    if (buffer && buffer->length > 0) {
+    if (buffer) {
       soup_buffer_free (buffer);
     }
   }
